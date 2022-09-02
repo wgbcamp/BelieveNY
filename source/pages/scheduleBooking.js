55802bcee208aeae73d2906878aeec330c265bbd
Warren Brown <warrengbrn@gmail.com>
2022-09-01 22:12:07 -0400

    calendar now properly fills out dates based on local time when switching to different months and years

--- source/pages/scheduleBooking.js
+++ source/pages/scheduleBooking.js
@@ -1,16 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/scheduleBooking.css'
 
-const timeValues = ["Morning", "Afternoon", "Evening"];
-const morningTimes = ["9:00 am", "10:00 am", "11:00 am"];
-const afternoonTimes = ["12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm"];
-const eveningTimes = ["6:00 pm", "7:00 pm", "8:00 pm"];
+const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-
-
-//gets all the values of the days of the year
+//gets all the values of the days of the month
 function monthDays(year, month){
 
     const date = new Date(year, month, 1);
@@ -23,61 +16,99 @@ function monthDays(year, month){
     return dates;
 }
 const now = new Date();
-console.log(now.toDateString().slice(0,3));
+var actualDate = now.toDateString();
+var customMonth = month.indexOf(actualDate.slice(4,7));
+var customYear = actualDate.slice(-4);
 
-//finds starting day to place on calendar
-var todayValue;
-const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-for (var i=0; i<week.length; i++){
-    if(now.toDateString().slice(0,3) === week[i]){
-        todayValue = i;
-    }
-}
 
-//pushes all the days of the current month into array
-var thisMonth = monthDays(now.getFullYear(), now.getMonth());
-console.log(thisMonth);
 
-//pushes the end of the previous month into the start of the thisMonth array equivalent to the open calendar spaces
-var prevMonth = monthDays(now.getFullYear(), now.getMonth()-1).slice(-todayValue);
-console.log(prevMonth);
-thisMonth = prevMonth.concat(thisMonth);
-console.log(thisMonth);
-
-//pushes the start of the next month to the end of the thisMonth array equivalent to the open calendar spaces
-var nextMonth = monthDays(now.getFullYear(), now.getMonth()+1).slice(0,42-thisMonth.length);
-console.log(nextMonth);
-thisMonth = thisMonth.concat(nextMonth);
-console.log(thisMonth);
 
 const scheduleBooking = () => {
 
-    var [currMonth, newMonValue] = useState(month[new Date().getMonth()]);
-    var [currYear, newYrValue] = useState(new Date().getFullYear());
+    const timeValues = ["Morning", "Afternoon", "Evening"];
+    const morningTimes = ["9:00 am", "10:00 am", "11:00 am"];
+    const afternoonTimes = ["12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm"];
+    const eveningTimes = ["6:00 pm", "7:00 pm", "8:00 pm"];
+    
+    var [currMonth, changeMonth] = useState(0);
+    var [currYear, changeYear] = useState(0);
 
-    function changeMonth(data){
-
-        var value = 1;
-        if (data === "minus"){
-            value = -1;
+    function fillCalendar(data){
+        if(data === 1){
+            if(customMonth<11){
+                customMonth +=1;
+            }else{
+                customMonth = 0;
+                customYear++;
+            }
         }
-        
-        var arrayValue = month.indexOf(currMonth)+value;
-        value = month[arrayValue];
-        if (arrayValue > 11){
-            value = month[0];
-            newYrValue(currYear + 1);
+        if(data === -1){
+            if(customMonth>0){
+                customMonth -=1;
+            }else{
+                customMonth = 11;
+                customYear--;
+            }
         }
-        
-        if (arrayValue < 0){
-            value = month[11]
-            newYrValue(currYear - 1);
+        if(data === 0){
+            console.log("yes")
+            customMonth = month.indexOf(actualDate.slice(4,7));
+            customYear = actualDate.slice(-4);
         }
 
-
-        console.log(value);
-        newMonValue(value);
+    //finds starting day to place on calendar
+    var todayValue;
+    const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    var startDateArray = monthDays(customYear, customMonth);
+    for (var i=0; i<startDateArray.length; i++){
+        if (startDateArray[i].slice(8,10) === "01"){
+            //changes the name of the month on the calendar
+            changeMonth(startDateArray[i].slice(4,7));
+            changeYear(startDateArray[i].slice(10,15));
+            //decides the todayValue, used to choose how many days of the prev and next month to gather
+            for (var a=0; a<week.length; a++){
+                if (startDateArray[i].slice(0,3) === week[a]){
+                    todayValue = a;
+                }
+            }
+        }
     }
+
+    //pushes all the days of the current month into array
+    var thisMonth = monthDays(customYear, customMonth);
+
+
+    //pushes the end of the previous month into the start of the thisMonth array equivalent to the open calendar spaces
+    if(todayValue > 0){
+        if (customMonth === 0){
+            var prevMonth = monthDays(customYear-1, 11).slice(-todayValue);
+        }else{
+            var prevMonth = monthDays(customYear, customMonth-1).slice(-todayValue);
+        }
+        thisMonth = prevMonth.concat(thisMonth);
+    }
+    
+
+    //pushes the start of the next month to the end of the thisMonth array equivalent to the open calendar spaces
+    if (customMonth === 11){
+        var nextMonth = monthDays(customYear+1, 0).slice(0,42-thisMonth.length);
+    }else{
+        var nextMonth = monthDays(customYear, customMonth+1).slice(0,42-thisMonth.length);
+    }
+    thisMonth = thisMonth.concat(nextMonth);
+
+    newCalValue(thisMonth.map((value) => (
+        <div className="calTile">{value.slice(8,10)}</div>
+    )));  
+}
+    useEffect(() => {
+        
+    fillCalendar();
+
+    }, [])
+    
+
+    var [calendarValue, newCalValue] = useState("");
 
     return(
         <div>
@@ -89,11 +120,12 @@ const scheduleBooking = () => {
                     <div className="calendarAppGrid">
                         <div className="appCol1">
                             <div className="mtSwitchGrid padBottom40">
+                                {/* <div className="size22Font">{currMonth} {currYear}</div> */}
                                 <div className="size22Font">{currMonth} {currYear}</div>
-                                <div className="mtArrow" onClick={() => changeMonth("minus")}><i className="fa-solid fa-caret-left fa-xl"></i></div>
-                                <div className="mtArrow" onClick={() => changeMonth("plus")}><i className="fa-solid fa-caret-right fa-xl"></i></div>
+                                <div className="mtArrow" onClick={() =>fillCalendar(-1)}><i className="fa-solid fa-caret-left fa-xl"></i></div>
+                                <div className="mtArrow" onClick={() => fillCalendar(1)}><i className="fa-solid fa-caret-right fa-xl"></i></div>
                                 <div></div>
-                                <div className='mtToday'>Today</div>
+                                <div className='mtToday' onClick={() => fillCalendar(0)}>Today</div>
                             </div>
                         
                             <div className="dayGrid padBottom20">
@@ -106,9 +138,7 @@ const scheduleBooking = () => {
                                 <b>Sat.</b>
                             </div>
                             <div className="calendar">
-                                {thisMonth.map((value) => (
-                                    <div className="calTile">{value.slice(8,10)}</div>
-                                ))}
+                             {calendarValue}
                                 
                             </div>
                             <div className='dayPeriodGrid'>
