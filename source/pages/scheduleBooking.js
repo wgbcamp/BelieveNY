51ab0703f73deae347365890a7e48f6ba72a06d9
Warren Brown <warrengbrn@gmail.com>
2022-09-02 03:03:01 -0400

    dates that are past the buffer window are now invalid

--- source/pages/scheduleBooking.js
+++ source/pages/scheduleBooking.js
@@ -81,26 +81,98 @@ const scheduleBooking = () => {
     //pushes the end of the previous month into the start of the thisMonth array equivalent to the open calendar spaces
     if(todayValue > 0){
         if (customMonth === 0){
-            var prevMonth = monthDays(customYear-1, 11).slice(-todayValue);
+            var prevMonth = monthDays(parseInt(customYear)-1, 11).slice(-todayValue);
         }else{
             var prevMonth = monthDays(customYear, customMonth-1).slice(-todayValue);
         }
         thisMonth = prevMonth.concat(thisMonth);
     }
     
-
     //pushes the start of the next month to the end of the thisMonth array equivalent to the open calendar spaces
     if (customMonth === 11){
-        var nextMonth = monthDays(customYear+1, 0).slice(0,42-thisMonth.length);
+        var nextMonth = monthDays(parseInt(customYear)+1, 0).slice(0,42-thisMonth.length);
     }else{
         var nextMonth = monthDays(customYear, customMonth+1).slice(0,42-thisMonth.length);
     }
     thisMonth = thisMonth.concat(nextMonth);
 
+    //greys out dates before current day or within 24 hours
+    var invalidDays = [];
+
+    for (var i=0; i<thisMonth.length; i++){
+        var y = month.indexOf(thisMonth[i].slice(4,7));
+        var z = month.indexOf(actualDate.slice(4,7));
+        var e = week.indexOf(thisMonth[i].slice(0,3));
+        var r = week.indexOf(actualDate.slice(0,3));
+        var cMonth = month.indexOf(thisMonth[i].slice(4,7));
+        var aMonth = month.indexOf(actualDate.slice(4,7));
+        var cYear = thisMonth[i].slice(11,15);
+        var aYear = actualDate.slice(11,15);
+        var g = false;
+
+        //invalidates previous months
+        if ( y < z && (cYear <= aYear)){
+            g = true;
+        }
+        //invalidates previous day
+        if ( (e < r) && ( thisMonth[i+1].slice(0,15) === actualDate )){
+            g = true;
+        }
+        //invalidates current day
+        if (thisMonth[i].slice(0,15) === actualDate){
+            g = true;
+        }
+       
+        //invalidates previous years
+        if (cYear < aYear){
+            g = true;
+        }
+
+        //re-validates future dates 
+        if (cMonth > aMonth && (cYear >= aYear)){
+            g = false;
+        }
+
+        if (g === true){
+            invalidDays.push(thisMonth[i].slice(4,7).concat(thisMonth[i].slice(8,10)));
+        }
+    }
+
+    console.log("Here are days that must be greyed out:")
+    console.log(invalidDays);
+    console.log(thisMonth)
+    for (var i=0; i<invalidDays.length; i++){
+        thisMonth.splice(i, 1, invalidDays[i])
+    }
+    console.log(thisMonth);
+
+    //maps the values to the calendar tiles
     newCalValue(thisMonth.map((value) => (
-        <div className="calTile">{value.slice(8,10)}</div>
+
+        <div className={`calTile ${thisMonth.indexOf(value)} ${value.length > 5 ? value.slice(4,7).concat(value.slice(8,10)): "invalid"}`} onClick={() => toggleSelection(thisMonth.indexOf(value))} key={value.toString()}>{value.length > 5 ? value.slice(8,10) : value.slice(3,5)}</div>
     )));  
 }
+
+    function toggleSelection(data){
+        const containsCalTile = Array.from(document.getElementsByClassName("calTile"));
+        const containsClass = Array.from(document.getElementsByClassName(data));
+        const containsInvalid = Array.from(document.getElementsByClassName("invalid"));
+
+        containsCalTile.forEach(element => {
+            element.classList.remove("calTileSelected");
+        })
+
+        containsClass.forEach(element => {
+            console.log(containsClass);
+            element.classList.add("calTileSelected");
+        });
+
+        console.log(containsInvalid);
+        containsInvalid.forEach(element => {
+            element.classList.remove("calTileSelected");
+        })
+    }
+    
     useEffect(() => {
         
     fillCalendar();
