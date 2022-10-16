f0dd076c8ff0489141fe1ae5077ab481bd0f3703
Warren Brown <warrengbrn@gmail.com>
2022-10-16 05:27:14 -0400

    modified bottom contact banner styling to fix dynamic viewport height changes on mobile browsers, updated readme image

--- source/components/bottomContact.js
+++ source/components/bottomContact.js
@@ -4,14 +4,18 @@ import "../css/header.css";
 
 const bottomContact = (props) => {
 
+
+
     return(
         <div className="bottomContact">
-            <div>
-                <div><i className="fa-solid fa-envelope-open-text"></i> EMAIL GetHelp@BelieveNY.org</div>
-            </div>
-            <div className="blankSpace"></div>
-            <div> 
-                <div><i className="fa-solid fa-message"></i> TEXT 347-NYC-0011</div>
+            <div className="bottomContactGrid">
+                <div className="contact1">
+                    <div><i className="fa-solid fa-envelope-open-text"></i> EMAIL GetHelp@BelieveNY.org</div>
+                </div>
+                <div className="blankSpace"></div>
+                <div className="contact2"> 
+                    <div><i className="fa-solid fa-message"></i> TEXT 347-NYC-0011</div>
+                </div>
             </div>
         </div>
     )
