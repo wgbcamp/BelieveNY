01923ac79a1c13153f1baf62638b9f9016232816
Warren Brown <warrengbrn@gmail.com>
2022-12-31 21:08:48 -0500

    adjusted image slideshow to remain within bounds of array and slideshow component is ready to accept images from parent

--- source/components/slideshowBox.js
+++ source/components/slideshowBox.js
@@ -1,158 +1,16 @@
+import e from 'cors';
 import React, {useState} from 'react'
 
 const slideshowBox = (props) => {
 
-    const array = ["https://i.imgur.com/3jKvhFY.jpeg", "https://i.imgur.com/J2l4uLI.jpeg", "https://i.imgur.com/iaWr1P9.jpeg", "https://i.imgur.com/DuTbJYr.jpeg", "https://i.imgur.com/zfdFae1.jpeg", "https://i.imgur.com/cyyijEC.jpeg", "https://i.imgur.com/h0N8Pap.jpeg", "https://i.imgur.com/DVdcHmg.jpeg"];
-
-    function moveImage(direction){
-
-        //cases for css animations
-        if (props.content.leftCSS === "hold"){
-            if (direction === "left"){
-                props.updateLeftCSS("centerToRight");
-                props.updateRightCSS("leftToCenter");
-            }
-            if (direction === "right"){
-                props.updateLeftCSS("centerToLeft");
-                props.updateRightCSS("rightToCenter");
-            }
-        }else{
-            if (props.content.leftCSS === "centerToRight"){
-                if (direction === "left"){
-                    props.updateLeftCSS("leftToCenter");
-                    props.updateRightCSS("centerToRight");
-                }
-                if (direction === "right"){
-                    props.updateLeftCSS("rightToCenter");
-                    props.updateRightCSS("centerToLeft");
-                }
-            }
-            if (props.content.leftCSS === "leftToCenter"){
-                if (direction === "left"){
-                    props.updateLeftCSS("centerToRight");
-                    props.updateRightCSS("leftToCenter");
-                }
-                if (direction === "right"){
-                    props.updateLeftCSS("centerToLeft");
-                    props.updateRightCSS("rightToCenter");
-                }
-            }
-            if (props.content.leftCSS === "centerToLeft"){
-                if (direction === "left"){
-                    props.updateLeftCSS("leftToCenter");
-                    props.updateRightCSS("centerToRight");
-                }
-                if (direction === "right"){
-                    props.updateLeftCSS("rightToCenter");
-                    props.updateRightCSS("centerToLeft");  
-                }
-            }
-            if (props.content.leftCSS === "rightToCenter"){
-                if (direction === "left"){
-                    props.updateLeftCSS("centerToRight");
-                    props.updateRightCSS("leftToCenter");
-                }
-                if (direction === "right"){
-                    props.updateLeftCSS("centerToLeft");
-                    props.updateRightCSS("rightToCenter");
-                }
-            }
-        }
-
-        //track array intervals for each image source
-        if (direction === "left"){
-            if (props.content.currentSelection === ""){
-                props.updateRight(array.length-1);
-                props.updateSelection("left");
-            }
-            
-            if (props.content.currentSelection === "left"){
-                if (props.content.preventChange === false){
-                    props.updatePC(true);
-                }else{
-                    props.updateLeft(props.content.rightIndex - 1);
-                }
-                props.updateSelection("right");
-            }
-
-            if (props.content.currentSelection === "right"){
-                if (props.content.preventChange === true){
-                    props.updateLeft(props.content.leftIndex - 2);
-                    props.updatePC(false);
-                }else{
-                    if (props.content.leftIndex === 0){
-                        props.updateRight(array.length-1);
-                    }else{
-                        props.updateRight(props.content.leftIndex - 1);
-                    }
-                }
-                props.updateSelection("left");
-            }
-
-        }
-
-        if (direction === "right"){
-            if (props.content.currentSelection === ""){
-                props.updateRight(1);
-                props.updateSelection("right");
-            }
-
-            if (props.content.currentSelection === "left"){
-                props.updateRight(props.content.leftIndex + 1);
-                props.updateSelection("right");
-            }
-
-            if (props.content.currentSelection === "right"){
-                if (props.content.rightIndex === array.length - 1){
-                    props.updateLeft(0);
-                }else{
-                    props.updateLeft(props.content.rightIndex + 1);
-                }
-                props.updateSelection("left");
-            }
-        }
-
-
-
-    }
-
-    function goRight(){
-        
-        console.log(activeImage);
-        if (activeImage === true){
-            updateImage2("moveItLeft");
-            updateImage1("returnFromRight");
-        }
-
-        if (activeImage === false){
-            updateI1(5);
-            updateImage1("moveItLeft");
-            updateImage2("returnFromRight");
-            
-        }
-        updateAI(!activeImage);
-        console.log("i1: " + i1);
-        console.log("i2: " + i2);
-        updateCC(clickCount+1);
-        console.log("clickCount: " + clickCount);
-    }
-
-    function goLeft(){
-        console.log(activeImage);
-        if (activeImage === true){
-            updateImage2("moveItRight");
-            updateImage1("returnFromLeft");
-        }
-
-        if (activeImage === false){
-            updateImage1("moveItRight");
-            updateImage2("returnFromLeft");
-        }
-        updateAI(!activeImage);
-    }
+    const array = props.content.images;
 
     function interval(data){
-            updateCC(clickCount + data);
+            if ((clickCount === 0 && data === -1) || (clickCount === array.length-1 && data === 1)){
+                
+            }else{
+                updateCC(clickCount + data);
+            }
     }
 
     var [activeImage, updateAI] = useState(true);
@@ -161,7 +19,6 @@ const slideshowBox = (props) => {
     var [i1, updateI1] = useState(0);
     var [i2, updateI2] = useState("");
     var [clickCount, updateCC] = useState(0);
-    var [initialState, updateIS] = useState(true);
 
     return(    
             <div className={`ssBoxContainer border${props.content.series}`}>
@@ -178,19 +35,19 @@ const slideshowBox = (props) => {
                 </div>
                 <div className={`imageGrid`}>
                     <div className="igFlex">
-                        <div className="igArrowStyle" onClick={() => {interval(-1); updateIS(false);}}>
-                            <i class="fa-solid fa-arrow-left fa-2xl"></i>
+                        <div className="igArrowStyle" onClick={() => interval(-1)}>
+                            <i className="fa-solid fa-arrow-left fa-2xl"></i>
                         </div>
                     </div>
                     <div></div>
                     <div className="igFlex">
-                        <div className="igArrowStyle" onClick={() => {interval(1); updateIS(false);}}>
-                            <i class="fa-solid fa-arrow-right fa-2xl"></i>
+                        <div className="igArrowStyle" onClick={() => interval(1)}>
+                            <i className="fa-solid fa-arrow-right fa-2xl"></i>
                         </div>
                     </div>
                     <div className="imageSlideContainer delayVisibility opacityZero">
                         {array.map((value, i) => (
-                            <img src={array[i]} className={`imageNumber1 ${clickCount < i ? "setTransparency" : "removeTransparency"}`}></img>
+                            <img src={array[i]} className={`imageNumber1 ${clickCount < i ? "setTransparency" : "removeTransparency"}`} key={i}></img>
                         ))}
                     </div>
                 </div>
