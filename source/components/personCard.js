1c1b0dc7a1a5c6bd168881a63d61e914a4707adb
Warren Brown <warrengbrn@gmail.com>
2023-04-05 21:57:21 -0400

    updated aboutUs page with new content and styling

--- source/components/personCard.js
+++ source/components/personCard.js
@@ -7,15 +7,10 @@ const personCard = (props) => {
     return(
         <div className="cardFlex">
             <div className="cardContainer">
-                {descriptStatus ? <div className="cardDescriptor">
-                    <div className="cardText">{props.aboutUs.story}
-                    </div>
-                    </div> : <img className="person" src={props.aboutUs.photo} alt="photo"></img>}
-                
+                <img className="person" src={props.aboutUs.photo} alt="photo"></img>  
                 <div className="infoGrid">
                 <div className="name">{props.aboutUs.name}</div>
                 <div className="title">{props.aboutUs.title}</div>
-                <div className="moreAbout" onClick={() => toggleDS(!descriptStatus)}>More about me ^</div>
                 </div>
             </div>
         </div>
