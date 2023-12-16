fdca6eab2a09054cab5e23f441a99591eeb4b1bd
Warren Brown <56795202+wgbcamp@users.noreply.github.com>
2023-12-16 10:51:00 -0500

    Update impactReport.js
    updated content on impact report page

--- source/pages/impactReport.js
+++ source/pages/impactReport.js
@@ -48,21 +48,45 @@ const impactReport = () => {
                         <ul>
                             <li>Fed over 100 people at our “Spread the Love” Valentine’s Day event in Tompkins Sq Park
                                 <ul>
-                                    <li>
-                                    During this same event, we handed out dozens of items of PPE, hand sanitizer, and hand warmers to event attendees
-                                    </li>
+                                    <li>During this same event, we handed out dozens of items of PPE, hand sanitizer, and hand warmers to event attendees</li>
                                 </ul>
                             </li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>In early March, partnered with The Ellen Macguire Foundation to serve approximately 30 people experiencing homelessness in Midtown Manhattan food and warm clothing</li>
+                            <li>
+                                <ul>
+                                    <li>During this same event, we handed out dozens of items of PPE, hand sanitizer, and hand warmers to event attendees</li>
+                                </ul>
+                            </li>
+                            <li>Hosted a community clean up in Bushwick (Myrtle-Wyckoff), Brooklyn on March 21</li>
+                            <li>Hosted a community clean up in St. Mary’s Park, South Bronx on April 25th</li>
+                            <li>Executive Director, Assistant Director, and several volunteers participated in the week long Global Mental Health Summer Institute Training at Columbia University</li>
+                            <li>Fed and clothed over 100 people experiencing homelessness in Washington Sq Park at our June 13th event</li>
+                            <li>At this same event, we partnered with a local hair salon and gave out over 2 dozen haircuts to people experiencing homelessness</li>
+                            <li>Fed over 75 people at a meal hand out in Lincoln Center on Sept 21. The traffic was a bit slow at Lincoln Center, so several of our volunteers packed up the food and gave it out to people experiencing homelessness at Penn Station and Bryant Park</li>
+                            <li>
+                                <ul>
+                                    <li>During this same event, we handed out dozens of items of PPE, hand sanitizer, and hand warmers to event attendees</li>
+                                </ul>
+                            </li>
+                            <li>Hosted a community clean up in Hylan Park, The Bronx on Oct 3rd</li>
+                            <li>Served over 80 hot meals and hundreds of warm clothing items to people experiencing homelessness during our Thanksgiving meal and clothing hand out on November 28 in Tompkins Sq Park</li>
+                            <li>
+                                <ul>
+                                    <li>During this same event, we handed out dozens of items of PPE, hand sanitizer, and hand warmers to event attendees</li>
+                                </ul>
+                            </li>
+                            <li>Delivered 30 children and their single mothers living in shelters, or transitional homes, holiday homes holiday gifts, totaling 42 gifts</li>
                         </ul>
                        <div>2020</div>
                        <ul>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
+                            <li>Fed over 75 people experiencing homelessness at Herbert Von King Park in Brooklyn at our Thanksgiving Day meal hand out</li>
+                            <li>
+                                <ul>
+                                    <li>During this same event, we handed out dozens of items of PPE, hand sanitizer, and hand warmers to event attendees</li>
+                                </ul>
+                            </li>
+                            <li>Registered over 200 people to vote in underserved areas in Brooklyn, Manhattan, Queens, and The Bronx, majority of people who were first time votes and people of color 
+                            </li>
                         </ul>
                     </div>
                 </div>
