e28a411fdd3e6a94bc839c303f165ab5f210d273
Warren Brown <warrengbrn@gmail.com>
2024-08-18 04:43:59 -0400

    updated newHome3.js

--- source/pages/newHome3.js
+++ source/pages/newHome3.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import { Container, Row, Col, Image} from 'react-bootstrap';
+import { Container, Row, Col, Image, Carousel } from 'react-bootstrap';
 
 import custom from '../css/home3.module.css';
 
 import everyone from '../images/general/IMG_5998.jpg'
+import foodDelivery from '../images/events/foodDelivery.jpeg'
+import donuts from '../images/events/donuts.jpeg'
+import servingFood from '../images/general/ServingFood.jpg'
+import '../images/general/ServingFood.jpg'
+
 
 const newHome3 = () => {
     return(
         <div>
-            {/* <div className="simpleFlex">
-                <div className="maxWidth width80Per">
-                </div>
-            </div> */}
-w
             <Container>
                 <Row>
                     <Col xl={6} className="mb-4">
@@ -33,45 +33,106 @@ w
                         <Image src={everyone} className='mt-5' fluid rounded/>  
                     </Col>
                 </Row>
+            </Container>
+            <div className={`${custom.backgroundColor2} text-light mt-5 pb-5`}>
+                <Container>
+                    <Row>
+                        <Col className={`${custom.huge} text-center`}>
+                            Our Services
+                        </Col>
+                    </Row>
+                    <Row >
+                        <Col xl={6}>
+                            <Row className={`mt-5 mx-1 ${custom.backgroundColor2} bg-gradient text-white rounded-2`}>
+                                <Col>
+                                    <Row className={custom.huge}>
+                                        <Col>
+                                            The Open Space
+                                        </Col>
+                                    </Row>
+                                    <Row className="fs-2 mt-5 mb-5">
+                                        <Col>   
+                                            100% free of charge and confidential, the Open Space offers a safe and nonjudgemental place to talk about what you are going through while remaining anonymous.
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col xl={6}>
+                            <Row className={`mt-5 mx-1 ${custom.backgroundColor2} bg-gradient text-white rounded-2`}>
+                                <Col>
+                                    <Row className={custom.huge}>
+                                        <Col>
+                                            Get Help
+                                        </Col>
+                                    </Row>
+                                    <Row className="fs-2 mt-5 mb-5">
+                                        <Col>   
+                                            100% free of charge and confidential, the Open Space offers a safe and nonjudgemental place to talk about what you are going through while remaining anonymous.
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <Container>
                 <Row>
                     <Col className={`${custom.huge} text-center`}>
-                        Our Services
+                        Community Events
                     </Col>
                 </Row>
+                {/* <Row>
+                    <Col>
+                        <Carousel fade sm={6}>
+                            <Carousel.Item>
+                                <Image src={donuts} className='mt-5' fluid rounded/>  
+                                <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={foodDelivery} className='mt-5' fluid rounded/>  
+                                <Carousel.Caption>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={servingFood} className='mt-5' fluid rounded/>  
+                                <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>
+                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                                </p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel> 
+                    </Col>
+                </Row>  */}
                 <Row>
-                    <Col xl={6}>
-                        <Row className={`mt-5 mx-1 ${custom.backgroundColor1} bg-gradient text-white rounded-2`}>
+                    <Col>
+                        <Row>
                             <Col>
-                                <Row className={custom.huge}>
-                                    <Col>
-                                        The Open Space
-                                    </Col>
-                                </Row>
-                                <Row className="fs-2 mt-5 mb-5">
-                                    <Col>   
-                                        100% free of charge and confidential, the Open Space offers a safe and nonjudgemental place to talk about what you are going through while remaining anonymous.
-                                    </Col>
-                                </Row>
+                                Col 1
+                            </Col>
+                            <Col>
+                                Col 2
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                Col 3
+                            </Col>
+                            <Col>
+                                Col 4
                             </Col>
                         </Row>
                     </Col>
-                    <Col xl={6}>
-                        <Row className={`mt-5 mx-1 ${custom.backgroundColor2} bg-gradient text-white rounded-2`}>
-                            <Col>
-                                <Row className={custom.huge}>
-                                    <Col>
-                                        Get Help
-                                    </Col>
-                                </Row>
-                                <Row className="fs-2 mt-5 mb-5">
-                                    <Col>   
-                                        100% free of charge and confidential, the Open Space offers a safe and nonjudgemental place to talk about what you are going through while remaining anonymous.
-                                    </Col>
-                                </Row>
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>   
+                </Row>
+
             </Container>
         </div>
     )
